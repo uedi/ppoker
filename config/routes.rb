d24c97f1633fe1e91e0d0f8812cfd7b37480c30a
uedi <uedi@users.noreply.github.com>
2017-04-10 13:41:51 +0300

    users,teams,memberships routes+controllers+index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  resources :users, only: [:index]
+  resources :teams, only: [:index]
+  resources :memberships, only: [:index]
+  
   root 'home#index'
   
 end
