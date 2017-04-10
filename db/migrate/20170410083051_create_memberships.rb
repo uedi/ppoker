f6b75a681c8e403725029ecaa85adf68687c4046
uedi <uedi@users.noreply.github.com>
2017-04-10 11:32:02 +0300

    db: memberships

---
+++ db/migrate/20170410083051_create_memberships.rb
@@ -0,0 +1,9 @@
+class CreateMemberships < ActiveRecord::Migration[5.0]
+  def change
+    create_table :memberships do |t|
+      t.integer :user_id
+      t.integer :team_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410082710) do
+ActiveRecord::Schema.define(version: 20170410083051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string   "name"
