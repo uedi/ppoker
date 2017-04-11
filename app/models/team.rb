c7ef6b3b901493973b4d097e5faf5910f2209702
uedi <uedi@users.noreply.github.com>
2017-04-11 21:31:02 +0300

    team page shows users invited

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,6 @@ class Team < ApplicationRecord
 
   has_many :memberships, dependent: :destroy
   has_many :members, -> { where(memberships: {accepted: true}) }, through: :memberships, source: :user
+  has_many :users_invited, -> { where(memberships: {accepted: false}) }, through: :memberships, source: :user
 
 end
